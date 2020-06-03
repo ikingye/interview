2c2bcec6e65952724f9509d20bd339a7366ac3f7
yewang <yewang@baidu.com>
2020-06-04 01:56:24 +0800

    rebuilding site Thu Jun  4 01:56:24 CST 2020

--- deploy.sh
+++ deploy.sh
@@ -12,16 +12,7 @@ git submodule update --remote
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To docs folder
-cd docs
-# Add changes to git.
-git add .
-git commit -m "$msg"
-# Push to blog repo and trigger building blog.
-git push origin master
-
 # push to source repo
-cd ..
 git add .
 git commit -m "$msg"
 git push origin master
